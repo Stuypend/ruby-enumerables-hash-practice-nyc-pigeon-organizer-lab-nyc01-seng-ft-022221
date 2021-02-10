b4145c5ee1bd6979d8338cb2685bab6297e6ca20
Andre Powers <andre@stuypend.com>
2021-02-10 17:52:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,10 @@
 def nyc_pigeon_organizer(data)
-  name = []
+  pigeons = {}
   data.each_with_object({}) do |(key, value), output|
-    value.each do |inner_key_color, names|
+    value.each do |inner_key, names|
       names.each do |n|
-        if(name.find_index(n) == nil)
-          name.push(n)
-        end
+        pigeons[n][key] = []
+        pigeons[n][key].push(inner_key.to_s)
       end
     end
   end
@@ -13,4 +12,23 @@ def nyc_pigeon_organizer(data)
   # write your code here!
 end
 
-puts :people.to_s
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+puts nyc_pigeon_organizer(pigeon_data)
