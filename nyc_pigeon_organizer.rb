ae310b0b86fbbe7e22acf2dea56997e7bf81f2c8
Andre Powers <andre@stuypend.com>
2021-02-10 15:45:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,8 @@
 def nyc_pigeon_organizer(data)
+  hash = :purple => ["Theo", "Peter Jr.", "Lucky"]
+  puts hash[data]
   # write your code here!
 end
+
+string = ":pruple"
+nyc_pigeon_organizer(string)
